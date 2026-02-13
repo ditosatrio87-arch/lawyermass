0087d28d280314d27f5fb6f154384bc5ea48dd59
ditosatrio87-arch <ditosatrio87@gmail.com>
2026-02-13 17:53:41 +0700

    Change hero background image from PNG to JPG

--- src/app/pages/Beranda.jsx
+++ src/app/pages/Beranda.jsx
@@ -1,7 +1,7 @@
 import { ArrowRight } from 'lucide-react';
 import { Button } from '../components/ui/button';
 import { Link } from 'react-router-dom';
-import heroBackground from '/hero.png';
+import heroBackground from '/hero.jpg';
 import { VisiMisi } from '../components/VisiMisi';
 
 export function Beranda() {
