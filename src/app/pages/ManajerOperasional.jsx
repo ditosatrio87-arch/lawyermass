e5cdfd9cf89cd149ca49f92a465c0daad3c61229
ditosatrio87-arch <ditosatrio87@gmail.com>
2026-02-23 23:54:52 +0700

    Update manager image in ManajerOperasional component

--- src/app/pages/ManajerOperasional.jsx
+++ src/app/pages/ManajerOperasional.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { motion, AnimatePresence } from 'motion/react';
 import { Card } from '../components/ui/card';
 import { Plus, X } from 'lucide-react';
-import managerImage from "/placeholder.png";
+import managerImage from "/dzul.jpeg";
 
 export function ManajerOperasional() {
   const [hoveredIndex, setHoveredIndex] = useState(null);
